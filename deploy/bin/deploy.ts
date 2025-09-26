e4b77c97f8b3fef3f504b323dddbc857630a6892
bulingen <adamlecorney@gmail.com>
2025-09-26 21:28:19 +0200

    feat: start of static site stack

--- deploy/bin/deploy.ts
+++ deploy/bin/deploy.ts
@@ -1,20 +1,11 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { DeployStack } from '../lib/deploy-stack';
+import { StaticSite } from '../lib/static-site-stack';
+
+const account = "897469443626"
+const region = "eu-west-1"
 
 const app = new cdk.App();
-new DeployStack(app, 'DeployStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+new StaticSite(app, 'RuffenStaticSite', {
+  env: { account, region }
 });
