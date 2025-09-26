b8452224d708d1c5e824b8444c4c5a9f4be2b44c
bulingen <adamlecorney@gmail.com>
2025-09-26 23:38:42 +0200

    feat: create gh actions role

--- deploy/bin/deploy.ts
+++ deploy/bin/deploy.ts
@@ -1,11 +1,20 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { StaticSite } from '../lib/static-site-stack';
+import { GitHubActionsRoleStack } from '../lib/gh-actions-role-stack';
 
 const account = "897469443626"
 const region = "eu-west-1"
 
 const app = new cdk.App();
+
+new GitHubActionsRoleStack(app, 'RuffenGitHubActionsRole', {
+  githubOidcProviderArn: "arn:aws:iam::897469443626:oidc-provider/token.actions.githubusercontent.com",
+  githubRepo: 'bulingen/ruffen',
+  branch: 'main',
+  env: { account, region }
+});
+
 new StaticSite(app, 'RuffenStaticSite', {
   distFolder: '../dist',
   env: { account, region }
